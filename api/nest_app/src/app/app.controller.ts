8ef4b901ef0770d2e138bec5d4b75e6bfc7251dc
松本悠秀 <matsumotoyuho@matsumotoyuushuunoMacBook-Pro.local>
2023-07-03 23:06:30 +0900

    MV: ディレクトリを綺麗にするためにファイルを移動

---
+++ api/nest_app/src/app/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, HttpStatus, Query, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiResponse } from '@nestjs/swagger';
+import { GetRoomsResponse, GetRoomsRequest, GetUsersRequest } from '../dto/channel.dto';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/rooms')
+  @ApiResponse({ status: HttpStatus.OK, type: GetRoomsResponse })
+  getRooms(@Query() { user_id, username }: GetUsersRequest): GetRoomsResponse {
+    return { Rooms: [
+		{ id: user_id, name: username},
+		{ id: '2', name: 'DM_personA_personC'}] };
+  }
+}
