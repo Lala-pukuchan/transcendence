5fc63a5600925a96dce05fcfe6c51f92743a787d
松本悠秀 <matsumotoyuho@matsumotoyuushuunoMacBook-Pro.local>
2023-07-13 21:55:31 +0900

    UPD: createするときにUIを変更 #35

--- front/react_app/src/CreateRoom.tsx
+++ front/react_app/src/CreateRoom.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useEffect } from 'react';
 import { FormControl, InputLabel, Input, FormHelperText, FormLabel, RadioGroup, FormControlLabel, Radio, IconButton, OutlinedInput, InputAdornment, Box, Button } from '@mui/material';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import CheckIcon from '@mui/icons-material/Check';
 
 function CreateRoom() {
 
-  const [value, setValue] = useState("");
+  const [roomType, setroomType] = useState("");
+  const [accessLevel, setaccessLevel] = useState("");
+  const [password, setPassword] = useState("");
+  const [canSubmit, setCanSubmit] = useState(false);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue((event.target as HTMLInputElement).value);
+  const handleTypeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setroomType((event.target as HTMLInputElement).value);
+  };
+
+  const handleAccessChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setaccessLevel((event.target as HTMLInputElement).value);
   };
 
   const [showPassword, setShowPassword] = React.useState(false);
@@ -21,65 +29,93 @@ function CreateRoom() {
     event.preventDefault();
   };
 
+  const isInputComplete = () => {
+    return roomType == "room" && accessLevel && (accessLevel !== "protected" || password);
+  };
+  
+
+  // 正規表現を使って、入力値が英語の文字または一部の記号であることを確認します。
+  const handlePasswordChange = (event) => {
+      const newValue = event.target.value;
+       if (/^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]*$/.test(newValue)) {
+      setPassword(newValue);
+      } else {
+      alert("Passwords must be alphanumeric characters or some symbols");
+      }
+    };
+
   return (
     <>
       <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
         <FormControl>
           
-          <InputLabel htmlFor="my-input">Room Name</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
-          <FormHelperText id="my-helper-text">Please input roomName.</FormHelperText>
+          {roomType === "room" && (
+            <div>
+                <InputLabel htmlFor="my-input">Room Name</InputLabel>
+                <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">Please input roomName.</FormHelperText>
+            </div>
+        )}
           
-          <FormLabel id="demo-controlled-radio-buttons-group" sx={{ m: 2 }}>Type1</FormLabel>
+          <FormLabel id="demo-controlled-radio-buttons-group" sx={{ m: 2 }}>Room Type</FormLabel>
           <RadioGroup
             aria-labelledby="demo-controlled-radio-buttons-group"
             name="controlled-radio-buttons-group"
-            value={value}
-            onChange={handleChange}
+            value={roomType}
+            onChange={handleTypeChange}
           >
             <FormControlLabel value="room" control={<Radio />} label="Room" />
             <FormControlLabel value="dm" control={<Radio />} label="DM" />
           </RadioGroup>
 
-          <FormLabel id="demo-controlled-radio-buttons-group" sx={{ m: 2 }}>Type2</FormLabel>
-          <RadioGroup
-            aria-labelledby="demo-controlled-radio-buttons-group"
-            name="controlled-radio-buttons-group"
-            value={value}
-            onChange={handleChange}
-          >
-            <FormControlLabel value="public" control={<Radio />} label="Public" />
-            <FormControlLabel value="private" control={<Radio />} label="Private" />
-            <FormControlLabel value="protected" control={<Radio />} label="Protected" />
-          </RadioGroup>
+          {roomType === "room" && (
+            <div>
+              <FormLabel id="demo-controlled-radio-buttons-group" sx={{ m: 2 }}>Access Level</FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={accessLevel}
+                onChange={handleAccessChange}
+              >
+                <FormControlLabel value="public" control={<Radio />} label="Public" />
+                <FormControlLabel value="private" control={<Radio />} label="Private" />
+                <FormControlLabel value="protected" control={<Radio />} label="Protected" />
+              </RadioGroup>
+            </div>
+          )}
 
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? 'text' : 'password'}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Password"
-            />
-            <FormHelperText id="my-helper-text2">Please input password when creating protected in Type2.</FormHelperText>
-          </FormControl>
-          
-          <Button variant="contained" endIcon={<AddCommentIcon />} sx={{ m: 2 }}>
-            CREATE ROOM
-          </Button>
+            {accessLevel === "protected" && roomType == "room" && (
+            <div>
+              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  value={password}
+                  onChange={handlePasswordChange}
+                  label="Password"
+                />
+                <FormHelperText id="my-helper-text2">You can protect your channel by password</FormHelperText>
+              </FormControl>
+            </div>
+          )}
         </FormControl>
       </Box>
+      <Button variant="contained" endIcon={<CheckIcon />} sx={{ m: 2 }} disabled={!isInputComplete()}>
+        Create
+      </Button>
     </>
   )
 }
