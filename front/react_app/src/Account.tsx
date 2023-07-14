e570061de61e0d869acabc291b60a7478ebd9404
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-07-14 20:12:42 +0900

    tfa event handler

--- front/react_app/src/Account.tsx
+++ front/react_app/src/Account.tsx
@@ -7,17 +7,19 @@ import { getCookie } from './utils/GetCookie.tsx';
 
 function Account() {
 
+	// tokenが無い場合、ログイン画面にリダイレクトさせる
 	if (!getCookie("token")) {
 		window.location.href = "login";
 		return null;
 	}
 
+	// ユーザー名
+	const username = localStorage.getItem('username');
+
 	// 二要素認証
 	const [tfaEnabled, setTfaEnabled] = useState(false);
 
 	useEffect(() => {
-
-		const username = localStorage.getItem('username');
 
 		httpClient
 			.get("/users/" + username)
@@ -30,6 +32,20 @@ function Account() {
 				console.log("error");
 			});
 	}, []);
+
+	const handleSwitchChange = async (event) => {
+		const { checked } = event.target;
+		//httpClient
+		//	.get("/users/" + username)
+		//	.then((response) => {
+		//		console.log("response: ", response);
+		//		setTfaEnabled(checked);
+		//	})
+		//	.catch(() => {
+		//		console.log("error");
+		//		setTfaEnabled(!checked);
+		//	});
+	};
 
 	return (
 		<>
@@ -56,7 +72,7 @@ function Account() {
 						</Stack>
 						<p>Name: ○○○</p>
 					<Divider sx={{ m:3 }}>Two Factor Authentication</Divider>
-						<FormControlLabel control={<Switch defaultChecked={tfaEnabled} />} label="Enable TFA" />
+						<FormControlLabel control={<Switch checked={tfaEnabled} />} onChange={handleSwitchChange} label="Enable TFA" />
 					<Divider sx={{ m:3 }}>Pong Game</Divider>
 						<Typography component="legend">Game Rating</Typography>
 						<Rating name="no-value" value={3} />
