ab47264acedd40e50ddb1235aee33e5cad065310
88-yana <85784752+88-yana@users.noreply.github.com>
2023-07-23 19:48:35 +0900

    ADD:demo emit(issue #50)

--- front/react_app/src/Game.tsx
+++ front/react_app/src/Game.tsx
@@ -2,7 +2,7 @@ import { Button, Box, Grid, TextField, Paper, Typography, Avatar } from '@mui/ma
 import { useRef, useEffect, useLayoutEffect, useCallback, useState } from 'react';
 import UndoIcon from '@mui/icons-material/Undo';
 import { useNavigate, useLocation } from 'react-router-dom';
-
+import io from 'socket.io-client';
 
 const paddleWidth: number = 20, paddleHeight: number = 200, ballWidth: number = 16, wallOffset: number = 20;
 const MATCHPOINT: number = 2;
@@ -107,112 +107,137 @@ class Ball extends Position {
 	}
 }
 
+const socket = io('http://localhost:3000');
+
 function Game() {
-	const navigate = useNavigate();
-	const canvasRef = useRef<HTMLCanvasElement | null>(null);
-	const contextRef = useRef<CanvasRenderingContext2D | null>(null);
-	const animationIdRef = useRef<number | null>(null);
-	const [playerScore, setPlayerScore] = useState(0);
-	const [opponentScore, setOpponentScore] = useState(0);
 
-	const [isAnimating, setIsAnimating] = useState(false);
+	useEffect(() => {
+		socket.on('connect', () => {
+		  // eslint-disable-next-line no-console
+		  console.log('connection ID : ', socket.id);
+		});
+	}, []);
 
-	useLayoutEffect(() => { //FIXME: 事故ったら, useEffectに戻す
-		const canvas = canvasRef.current;
-		const context = canvas.getContext('2d');
-		contextRef.current = context;
-
-		canvas.width = 1200;
-		canvas.height = 800;
-		canvas.style.width = (canvas.width / 2) + 'px';
-		canvas.style.height = (canvas.height / 2) + 'px';
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		context.fillStyle = '#429950';
-		context.fillRect(0, 0, canvas.width, canvas.height);
-		const player = new Paddle(paddleWidth, paddleHeight, wallOffset, canvas.height / 2 - paddleHeight / 2);
-		const opponent = new Paddle(paddleWidth, paddleHeight, canvas.width - wallOffset - paddleWidth, canvas.height / 2 - paddleHeight / 2);
-		const ball = new Ball(ballWidth, ballWidth, canvas.width / 2 - ballWidth / 2, canvas.height / 2 - ballWidth / 2);
-		player.draw(context);
-		opponent.draw(context);
-		console.log("init game");
-
-		const handleKeyDown = (event) => {
-			if (event.key === 'ArrowUp') {
-				player.update(canvas, -1);
-			}
-			else if (event.key === 'ArrowDown') {
-				player.update(canvas, 1);
-			}
-		};
-
-		const gameLoop = () => {
-			context.clearRect(0, 0, canvas.width, canvas.height);
-
-			ball.update(player, opponent, canvas, setPlayerScore, setOpponentScore);
-			player.update(canvas, 0);
-			setPlayerScore(prevScore => {
-				if (prevScore >= MATCHPOINT) {
-					setIsAnimating(false);
-				}
-				return prevScore;
-			});
-			setOpponentScore(prevScore => {
-				if (prevScore >= MATCHPOINT) {
-					setIsAnimating(false);
-				}
-				return prevScore;
-			});
-			// Draw game elements
-			context.fillStyle = '#429950';
-			context.fillRect(0, 0, canvas.width, canvas.height);
-			ball.draw(context);
-			player.draw(context);
-			opponent.draw(context);
-			animationIdRef.current = requestAnimationFrame(gameLoop);
-		}
-		if (isAnimating) {
-			animationIdRef.current = requestAnimationFrame(gameLoop);
-		}
-		else {
-			if (playerScore >= MATCHPOINT) {
-				alert("You Win!\n" + "Your Score: " + playerScore + " Opponent Score: " + opponentScore);
-			}
-			else if (opponentScore >= MATCHPOINT) {
-				alert("You Lose.\n" + "Your Score: " + playerScore + "  :  Opponent Score: " + opponentScore);
-			}
-		}
-		console.log(playerScore, opponentScore);
-		document.addEventListener('keydown', handleKeyDown);
-
-		return () => {
-			document.removeEventListener('keydown', handleKeyDown);
-			cancelAnimationFrame(animationIdRef.current);
-			setPlayerScore(0);
-			setOpponentScore(0);
-		};
-	}, [isAnimating]);
-
-	const handleStartStop = () => {
-		setIsAnimating(prevIsAnimating => !prevIsAnimating);
-	};
+	const onClickSubmit = useCallback(() => {
+		socket.emit('message', 'hello', socket.id);
+	}, []);
 
 	return (
 		<>
-			{/* <h2>Game</h2> */}
-			<div>
-				<h2>Game</h2>
-				{/* 他のコンポーネントやテキストなど */}
-				<h3>{playerScore} : {opponentScore}</h3>
-				<canvas ref={canvasRef}></canvas>
-				{/* 他のコンポーネントやテキストなど */}
-			</div>
-			<Button variant="contained" endIcon={<UndoIcon />} onClick={() => navigate('/')} sx={{ m: 2 }}>
-				Return Back
-			</Button>
-			<Button variant={isAnimating ? "contained" : "outlined"} onClick={handleStartStop}>
-				{isAnimating ? 'Reset' : 'Start'}
+		  <Button variant="contained" onClick={onClickSubmit} sx={{ m: 2 }}>
+				emit
 			</Button>
 		</>
 	);
 }
+
 export default Game
+
+// function Game() {
+// 	const navigate = useNavigate();
+// 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+// 	const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+// 	const animationIdRef = useRef<number | null>(null);
+// 	const [playerScore, setPlayerScore] = useState(0);
+// 	const [opponentScore, setOpponentScore] = useState(0);
+
+// 	const [isAnimating, setIsAnimating] = useState(false);
+
+// 	useLayoutEffect(() => { //FIXME: 事故ったら, useEffectに戻す
+// 		const canvas = canvasRef.current;
+// 		const context = canvas.getContext('2d');
+// 		contextRef.current = context;
+
+// 		canvas.width = 1200;
+// 		canvas.height = 800;
+// 		canvas.style.width = (canvas.width / 2) + 'px';
+// 		canvas.style.height = (canvas.height / 2) + 'px';
+// 		context.clearRect(0, 0, canvas.width, canvas.height);
+// 		context.fillStyle = '#429950';
+// 		context.fillRect(0, 0, canvas.width, canvas.height);
+// 		const player = new Paddle(paddleWidth, paddleHeight, wallOffset, canvas.height / 2 - paddleHeight / 2);
+// 		const opponent = new Paddle(paddleWidth, paddleHeight, canvas.width - wallOffset - paddleWidth, canvas.height / 2 - paddleHeight / 2);
+// 		const ball = new Ball(ballWidth, ballWidth, canvas.width / 2 - ballWidth / 2, canvas.height / 2 - ballWidth / 2);
+// 		player.draw(context);
+// 		opponent.draw(context);
+// 		console.log("init game");
+
+// 		const handleKeyDown = (event) => {
+// 			if (event.key === 'ArrowUp') {
+// 				player.update(canvas, -1);
+// 			}
+// 			else if (event.key === 'ArrowDown') {
+// 				player.update(canvas, 1);
+// 			}
+// 		};
+
+// 		const gameLoop = () => {
+// 			context.clearRect(0, 0, canvas.width, canvas.height);
+
+// 			ball.update(player, opponent, canvas, setPlayerScore, setOpponentScore);
+// 			player.update(canvas, 0);
+// 			setPlayerScore(prevScore => {
+// 				if (prevScore >= MATCHPOINT) {
+// 					setIsAnimating(false);
+// 				}
+// 				return prevScore;
+// 			});
+// 			setOpponentScore(prevScore => {
+// 				if (prevScore >= MATCHPOINT) {
+// 					setIsAnimating(false);
+// 				}
+// 				return prevScore;
+// 			});
+// 			// Draw game elements
+// 			context.fillStyle = '#429950';
+// 			context.fillRect(0, 0, canvas.width, canvas.height);
+// 			ball.draw(context);
+// 			player.draw(context);
+// 			opponent.draw(context);
+// 			animationIdRef.current = requestAnimationFrame(gameLoop);
+// 		}
+// 		if (isAnimating) {
+// 			animationIdRef.current = requestAnimationFrame(gameLoop);
+// 		}
+// 		else {
+// 			if (playerScore >= MATCHPOINT) {
+// 				alert("You Win!\n" + "Your Score: " + playerScore + " Opponent Score: " + opponentScore);
+// 			}
+// 			else if (opponentScore >= MATCHPOINT) {
+// 				alert("You Lose.\n" + "Your Score: " + playerScore + "  :  Opponent Score: " + opponentScore);
+// 			}
+// 		}
+// 		console.log(playerScore, opponentScore);
+// 		document.addEventListener('keydown', handleKeyDown);
+
+// 		return () => {
+// 			document.removeEventListener('keydown', handleKeyDown);
+// 			cancelAnimationFrame(animationIdRef.current);
+// 			setPlayerScore(0);
+// 			setOpponentScore(0);
+// 		};
+// 	}, [isAnimating]);
+
+// 	const handleStartStop = () => {
+// 		setIsAnimating(prevIsAnimating => !prevIsAnimating);
+// 	};
+
+// 	return (
+// 		<>
+// 			{/* <h2>Game</h2> */}
+// 			<div>
+// 				<h2>Game</h2>
+// 				{/* 他のコンポーネントやテキストなど */}
+// 				<h3>{playerScore} : {opponentScore}</h3>
+// 				<canvas ref={canvasRef}></canvas>
+// 				{/* 他のコンポーネントやテキストなど */}
+// 			</div>
+// 			<Button variant="contained" endIcon={<UndoIcon />} onClick={() => navigate('/')} sx={{ m: 2 }}>
+// 				Return Back
+// 			</Button>
+// 			<Button variant={isAnimating ? "contained" : "outlined"} onClick={handleStartStop}>
+// 				{isAnimating ? 'Reset' : 'Start'}
+// 			</Button>
+// 		</>
+// 	);
+// }
