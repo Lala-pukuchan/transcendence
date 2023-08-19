ab21e2e6e03a66f3e1d1318af187beaed2507926
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-08-19 16:49:37 +0900

    content type header

--- front/react_app/src/httpClient.ts
+++ front/react_app/src/httpClient.ts
@@ -13,9 +13,7 @@ httpClient.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
-  config.headers['Content-Type'] = 'application/json';
-
+  
   return config;
 }, (error) => {
   return Promise.reject(error);
